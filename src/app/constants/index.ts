3e77c8b2cf7db15de528c9b52f011ff438e2e802
Vitaliy Dorofeev <91981187+VitaliyDorofeev@users.noreply.github.com>
2024-06-23 08:40:43 +0300

    Delete src directory

--- src/app/constants/index.ts
+++
@@ -1,8 +0,0 @@
-const spreadsheetId = process.env.NX_SPREADSHEET_ID;
-const apiKey = process.env.NX_API_KEY;
-const range = 'Sheet1!A3:F';
-export const baseUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
-
-export default {
-  baseUrl,
-};
